cfbfee4fead0d4ed1d0af6936e582a61846e566d
peytonwterry <peytonwterry@gmail.com>
2024-10-08 14:05:28 -0400

    Looking good, cleaned up code, added printf instead of println

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,10 @@ public class Main {
         System.out.println("Welcome to Craps!\nThe rules are simple:");
         System.out.println("1. Roll Two Dice.\n2. If the sum is 2,3, or 12; You got craps or crapping out and you lose.");
         System.out.println("3. If the sum is 7 or 11 it is a natural and you win!");
-        System.out.println("4. For everything else, the sum becomes 'the point' and you keep rolling until you roll a 7 and lose or make the point sum and win!");
+        System.out.println("4. For everything else, the sum becomes 'the point' and you keep rolling until you roll a 7 and lose or make the point sum and win!\n");
+        System.out.println("Time to Roll!");
+
         while (!done) {
-            System.out.println("\nTime to Roll!");
             System.out.print("Press R to roll or Q to quit: ");
             playGame = in.nextLine();
             if (playGame.equalsIgnoreCase("Q")) {
@@ -27,33 +28,36 @@ public class Main {
                 die2 = gen.nextInt(6) + 1;
                 crapsRoll = die1 + die2;
                 if (crapsRoll == 2 || crapsRoll == 3 || crapsRoll == 12) {
-                    System.out.println(crapsRoll + "\n");
+                    System.out.printf("You rolled: %d and %d to make %d\n", die1, die2, crapsRoll);
                     System.out.println("Craps! You lost.");
                 } else if (crapsRoll == 7 || crapsRoll == 11) {
-                    System.out.println(crapsRoll + "\n");
+                    System.out.printf("You rolled: %d and %d to make %d\n", die1, die2, crapsRoll);
                     System.out.println("Natural! You won.");
                     done = true;
                 } else {//if (crapsRoll == 4 || crapsRoll == 5 || crapsRoll == 6 || crapsRoll == 8 || crapsRoll == 9 || crapsRoll == 10)
-                    System.out.println("The point is " + crapsRoll + "\n");
+                    System.out.printf("You rolled: %d and %d to make %d\n", die1, die2, crapsRoll);
+                    System.out.printf("The point is: %d\n", crapsRoll);
                     boolean continueGame = true;
 
                     while (continueGame) {
-                        System.out.print("Press R to roll or Q to quit: ");
+                        System.out.print("\nPress R to roll again or Q to quit: ");
                         nextRound = in.nextLine();
                         if (nextRound.equalsIgnoreCase("R")) {
                             die1 = gen.nextInt(6) + 1;
                             die2 = gen.nextInt(6) + 1;
                             roll2 = die1 + die2;
-                            System.out.println(roll2 + "\n");
 
                             if (roll2 == crapsRoll) {
+                                System.out.printf("\nYou rolled: %d and %d to make %d\n", die1, die2, roll2);
                                 System.out.println("You made the point sum! You win!");
                                 continueGame = false;
                             } else if (roll2 == 7) {
-                                System.out.println("You rolled a 7. You lose!");
+                                System.out.printf("\nYou rolled: %d and %d to make %d\n", die1, die2, roll2);
+                                System.out.println("You lose!");
                                 continueGame = false;
                             } else {
-                                System.out.println("Keep trying to get the point sum!");
+                                System.out.printf("\nYou rolled: %d and %d to make %d\n", die1, die2, roll2);
+                                System.out.printf("Keep trying to get the %d\n!", crapsRoll);
                             }
                         } else if (nextRound.equalsIgnoreCase("Q")) {
                             continueGame = false;
@@ -64,7 +68,7 @@ public class Main {
                     }
                 }
             do {
-                System.out.println("Would you like to play again? [Y/N]: ");
+                System.out.println("\nWould you like to play again? [Y/N]: ");
                 continueYN = in.nextLine();
                 if (continueYN.equalsIgnoreCase("Y")) {
                     done = false;
@@ -75,7 +79,7 @@ public class Main {
                 }
             } while (!continueYN.equalsIgnoreCase("Y") && !continueYN.equalsIgnoreCase("N"));
             } else {
-                System.out.println("Invalid input: " + playGame + "\nPlease select [R/Q]");
+                System.out.println("\nInvalid input: " + playGame + "\nPlease select [R/Q]\n");
             }
         }
     }
