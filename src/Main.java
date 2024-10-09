379a541c272485077320e92dccec44bc83f143db
ScurryTurry <92820704+ScurryTurry@users.noreply.github.com>
2024-10-09 14:21:09 -0400

    Betting for new branch

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ public class Main {
         String playGame = "";
         boolean done = false;
         int crapsRoll, die1, die2, roll2;
+        double playerBalance = 100.00;
+        double playerBet = 0.0;
 
         System.out.println("Welcome to Craps!\nThe rules are simple:");
         System.out.println("1. Roll Two Dice.\n2. If the sum is 2,3, or 12; You got craps or crapping out and you lose.");
@@ -18,6 +20,18 @@ public class Main {
         System.out.println("Time to Roll!");
 
         while (!done) {
+            if (playerBalance <= 0) {
+                System.out.println("You have no money to bet. Game over.");
+            }
+
+            System.out.printf("\nYour current balance is $%.2f. Enter your bet: $", playerBalance);
+            playerBet = in.nextDouble();
+            in.nextLine();
+
+            if (playerBet > playerBalance) {
+                System.out.println("You don't have enough money for that bet. Please bet an amount less than or equal to your balance.");
+            }
+
             System.out.print("Press R to roll or Q to quit: ");
             playGame = in.nextLine();
             if (playGame.equalsIgnoreCase("Q")) {
@@ -30,9 +44,11 @@ public class Main {
                 if (crapsRoll == 2 || crapsRoll == 3 || crapsRoll == 12) {
                     System.out.printf("\nYou rolled: %d and %d to make %d\n", die1, die2, crapsRoll);
                     System.out.println("Craps! You lost.");
+                    playerBalance -= playerBet;
                 } else if (crapsRoll == 7 || crapsRoll == 11) {
                     System.out.printf("\nYou rolled: %d and %d to make %d\n", die1, die2, crapsRoll);
                     System.out.println("Natural! You won.");
+                    playerBalance += (2 * playerBet);
                     done = true;
                 } else {//if (crapsRoll == 4 || crapsRoll == 5 || crapsRoll == 6 || crapsRoll == 8 || crapsRoll == 9 || crapsRoll == 10)
                     System.out.printf("\nYou rolled: %d and %d to make %d\n", die1, die2, crapsRoll);
@@ -50,10 +66,12 @@ public class Main {
                             if (roll2 == crapsRoll) {
                                 System.out.printf("\nYou rolled: %d and %d to make %d\n", die1, die2, roll2);
                                 System.out.println("You made the point sum! You win!");
+                                playerBalance += (2 * playerBet);
                                 continueGame = false;
                             } else if (roll2 == 7) {
                                 System.out.printf("\nYou rolled: %d and %d to make %d\n", die1, die2, roll2);
                                 System.out.println("You lose!");
+                                playerBalance -= playerBet;
                                 continueGame = false;
                             } else {
                                 System.out.printf("\nYou rolled: %d and %d to make %d\n", die1, die2, roll2);
@@ -68,6 +86,7 @@ public class Main {
                     }
                 }
             do {
+                System.out.printf("\nYour new balance is $%.2f.", playerBalance);
                 System.out.println("\nWould you like to play again? [Y/N]: ");
                 continueYN = in.nextLine();
                 if (continueYN.equalsIgnoreCase("Y")) {
